d077f32ff0ba3e723fa8adf57d586717f7851042
Tom Chen <30789036+tomkingchen@users.noreply.github.com>
2021-02-28 15:06:00 +1100

    Working script with OAuth2

--- module/BloggerAPI.ps1
+++ module/BloggerAPI.ps1
@@ -1,10 +1,30 @@
+# Get Google OAuth2 Token
+Function Get-OauthToken {
+  [CmdletBinding()]
+  param (
+      [Parameter(Mandatory = 1)]
+      [String]$ClientId,
+      [Parameter(Mandatory = 1)]
+      [String]$ClientSecret
+  )
+  
+  $client_id = $ClientId
+  $client_secret = $clientSecret
+  $scopes = "https://www.googleapis.com/auth/blogger"
+  Start-Process "https://accounts.google.com/o/oauth2/v2/auth?client_id=$client_id&scope=$scopes&access_type=offline&response_type=code&redirect_uri=urn:ietf:wg:oauth:2.0:oob"    
+  $code = Read-Host "Please enter the authorization code: "
+  $response = Invoke-WebRequest https://www.googleapis.com/oauth2/v4/token -ContentType application/x-www-form-urlencoded -Method POST -Body "client_id=$client_id&client_secret=$client_secret&redirect_uri=urn:ietf:wg:oauth:2.0:oob&code=$code&grant_type=authorization_code"
+  Return ($response.Content | ConvertFrom-Json).access_token
+}
+
 # List objects from Blogger API
 Function Get-BloggerObj {
   [CmdletBinding()]
   Param(
     [Parameter(Mandatory = 1)]
-    [String]$api_uri
+    [String]$ApiUri
   )
+  $api_uri = $ApiUri
   $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
   $headers.Add("Accept", "application/json")
   $headers.Add("Content-Type", "application/json")
@@ -23,11 +43,60 @@ Function Remove-BloggerObj {
   [CmdletBinding()]
   Param(
     [Parameter(Mandatory = 1)]
-    [String]$api_uri
+    [String]$ApiUri
   )
+  $api_uri = $ApiUri
   $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
   $headers.Add("Accept", "application/json")
   $headers.Add("Content-Type", "application/json")
   $web_response = Invoke-RestMethod $api_uri -Method 'DELETE' -Headers $headers
   return $web_response
+}
+
+# Check for SPAM comments in Blogger posts
+Function Get-SpamComments {
+  [CmdletBinding()]
+  param (
+      [Parameter(Mandatory = 1)]
+      [String]$BlogId,
+      [Parameter(Mandatory = 1)]
+      [String]$AccessToken
+  )
+
+  $blog_id = $BlogId
+  $uri = "https://blogger.googleapis.com/v3/blogs/"+ $blog_id
+  $access_token = "access_token=$AccessToken"
+  $post_uri = $uri + "/posts/?" + $access_token
+  $posts = Get-BloggerObj -ApiUri $post_uri
+  $bad_posts = @()
+
+  ForEach ($post in $posts){
+    # Check if post has comments/replies
+    if ($post.replies.totalItems -ne "0"){
+      $post_title = $post.title
+      $post_id = $post.id
+      $comments_uri = $uri + "/posts/$post_id/comments?" + $access_token
+      $comments = Get-BloggerObj -ApiUri $comments_uri
+      $bad_comments = @()
+      Foreach ($comment in $comments) {
+        $bad_comment = New-Object PSObject
+        $comment_content = $comment.content
+        # Check if comment contains hyper link
+        If ($comment_content -like "*href=*"){
+          $bad_comment| Add-Member -NotePropertyName 'id' -NotePropertyValue $comment.id
+          $bad_comment| Add-Member -NotePropertyName 'content' -NotePropertyValue $comment_content
+          $bad_comment| Add-Member -NotePropertyName 'link' -NotePropertyValue $comment.selflink
+          $bad_comments += $bad_comment
+        }
+      }
+      If ($bad_comments){
+        $bad_post = New-Object PSObject
+        $bad_post | Add-Member -NotePropertyName 'post_id' -NotePropertyValue $post_id
+        $bad_post | Add-Member -NotePropertyName 'post_title' -NotePropertyValue $post_title
+        $bad_post | Add-Member -NotePropertyName 'bad_comments' -NotePropertyValue $bad_comments
+        $bad_posts += $bad_post
+      }
+    }
+  }
+  return $bad_posts
 }
--- remove-spam.ps1
+++ remove-spam.ps1
@@ -3,20 +3,35 @@ param (
     [Parameter(Mandatory = 1)]
     [String]$BlogId,
     [Parameter(Mandatory = 1)]
-    [String]$APIKey
+    [String]$ClientId,
+    [Parameter(Mandatory = 1)]
+    [String]$ClientSecret
 )
 . ./module/BloggerAPI.ps1
 
-$bad_posts = ./check-spam.ps1 -BlogId $BlogId -APIKey $APIKey
-foreach ($bad_post in $bad_posts) {
-  $comments = $bad_post.bad_comments
-  foreach ($comment in $comments) {
-    $api_uri = $comment.link
-    Write-Host "Deleting " $api_uri
-    $api_uri += "?key=" + $APIKey
-    $result = Remove-BloggerObj -api_uri $api_uri
-    if (!$result){
-      Write-Host "Comment "$comment.id" Deleted"
+$token = Get-OauthToken -ClientId $ClientId -ClientSecret $ClientSecret
+$bad_posts = Get-SpamComments -BlogId $BlogId -AccessToken $token
+$bad_posts | Out-Host
+if ($bad_posts){
+  $yes = Read-Host "OK to delete these comments? [No/yes]: "
+  if ($yes -eq "yes"){
+    foreach ($bad_post in $bad_posts) {
+      $comments = $bad_post.bad_comments
+      foreach ($comment in $comments) {
+        $api_uri = $comment.link
+        Write-Host "Deleting " $api_uri
+        $api_uri += "?access_token=" + $token
+        $result = Remove-BloggerObj -ApiUri $api_uri
+        if (!$result){
+          Write-Host "Comment "$comment.id" Deleted"
+        }else{
+          Write-Host "[ERROR] Delete comment "$comment.id" Failed"
+        }
+      }
     }
+  }else {
+    Write-Host "Mission aborted!" -ForegroundColor Yellow
   }
+}else {
+  Write-Host "No SPAM comments found." -ForegroundColor Green
 }
